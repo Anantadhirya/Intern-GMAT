837c4f116d74bba0a2e5df79900dba49e6d96958
Anantadhirya <anantadhirya@gmail.com>
2022-11-13 14:31:12 +0700

    feat : connect backend

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
 import "./App.css";
 
 export default function App() {
+  const [socket, setSocket] = useState();
+
+  useEffect(
+    function () {
+      if (socket) return;
+      const socketio = io("http://13.113.187.150:5001");
+      console.log("connected");
+      setSocket(socketio);
+    },
+    [socket]
+  );
+
+  useEffect(
+    function () {
+      if (!socket) return;
+      socket.on("DATA", (payload) => {
+        console.log(payload);
+      });
+    },
+    [socket]
+  );
+
   return (
     <div>
       <div>Test</div>
